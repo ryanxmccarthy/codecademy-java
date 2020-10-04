7e0d84aa1789e2cb539dbd26a781a0f5c40106d8
Ryan XVX <ryanxmccarthy@gmail.com>
2020-10-04 12:12:16 -0700

    review

---
+++ variables/review.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Ryan";
+        int age = 27;
+        double desiredSalary = 120000.00;
+        char gender = 'm';
+        boolean lookingForJob = true;
+    }
+}
