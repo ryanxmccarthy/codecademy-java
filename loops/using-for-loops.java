13c00802c7e963638874f721e963a2254fef8d13
Ryan XVX <ryanxmccarthy@gmail.com>
2020-10-27 09:11:03 -0700

    using for loops

---
+++ loops/using-for-loops.java
@@ -0,0 +1,14 @@
+class Coffee {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+
+            System.out.println("Fry drinks cup of coffee #" + i);
+            i++;
+
+        }
+
+    }
+
+}
