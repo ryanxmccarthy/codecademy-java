26963ba7ecc9ea68613b730fdd5baddd0bd5f74e
Ryan XVX <ryanxmccarthy@gmail.com>
2020-10-29 09:21:34 -0700

    concat

---
+++ string-methods/concat.java
@@ -0,0 +1,14 @@
+public class CombineNames {
+
+    public static void main(String[] args) {
+
+        // Add your first names:
+        String firstName = "Ryan";
+        String lastName = "McCarthy";
+
+        // What's the full name?
+        System.out.println(firstName.concat(lastName));
+
+    }
+
+}
