e5f053c5bb1e95e295219f85129b7dc0613550e6
Ryan XVX <ryanxmccarthy@gmail.com>
2020-11-03 09:10:13 -0800

    runtime errors

---
+++ debugging/run-time-errors.java
@@ -0,0 +1,15 @@
+public class Debug {
+
+    public static void main(String[] args) {
+
+        int width = 20;
+        int length = 40;
+
+        // fix divison by zero
+        int ratio = length / width;
+
+        System.out.println(ratio);
+
+    }
+
+}
