768368de9ff10d4327fe058bc93bc1cee45508b6
Ryan XVX <ryanxmccarthy@gmail.com>
2020-11-04 09:49:59 -0800

    logic errors

---
+++ debugging/logic-errors.java
@@ -0,0 +1,16 @@
+public class Debug {
+
+    public static void main(String[] args) {
+
+        int steps = 10;
+
+        // was i = 0
+        for (int i = 1; i <= steps; i++) {
+
+            System.out.println("Step #" + i);
+
+        }
+
+    }
+
+}
